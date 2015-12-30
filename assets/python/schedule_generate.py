d554b98ddb7d7c1cd119331ae6e9ad7d427154a1
Andrew Famiglietti <afamiglietti@gmail.com>
2015-12-30 16:43:53 -0500

    add film.txt

--- assets/python/schedule_generate.py
+++ assets/python/schedule_generate.py
@@ -6,17 +6,17 @@ from datetime import timedelta
 from datetime import date
 f = open('syllabus_schedule.yml', 'w')
 
-theDay = date(2015, 8, 24)
+theDay = date(2016, 1, 13)
 week = 1
-while theDay < date(2015, 12, 7):
+while theDay < date(2016, 5, 2):
     now = theDay.isoformat()
     f.write("- date: " + now + '\n')
     f.write("  readings: \n")
     f.write("  in_class: \n")
     f.write("  assignment: \n")
     f.write("  assignment_link: \n\n")
-    if theDay.weekday() == 4:
-        increment = timedelta(days = 3)
+    if theDay.weekday() == 3:
+        increment = timedelta(days = 5)
     else:
         increment = timedelta(days = 2)
     theDay = theDay + increment
