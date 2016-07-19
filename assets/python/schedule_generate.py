95b3e6a8bf332b6990c3a73952608c3dd5e2a2b5
Andrew Famiglietti <afamiglietti@gmail.com>
2016-07-19 10:57:37 -0400

    updated landing

--- assets/python/schedule_generate.py
+++ assets/python/schedule_generate.py
@@ -4,19 +4,19 @@
 import time
 from datetime import timedelta
 from datetime import date
-f = open('syllabus_schedule.yml', 'w')
+f = open('syllabus_schedule2016F.yml', 'w')
 
-theDay = date(2016, 1, 13)
+theDay = date(2016, 8, 29)
 week = 1
-while theDay < date(2016, 5, 2):
+while theDay < date(2016, 12, 12):
     now = theDay.isoformat()
     f.write("- date: " + now + '\n')
     f.write("  readings: \n")
     f.write("  in_class: \n")
     f.write("  assignment: \n")
     f.write("  assignment_link: \n\n")
-    if theDay.weekday() == 3:
-        increment = timedelta(days = 5)
+    if theDay.weekday() == 4:
+        increment = timedelta(days = 3)
     else:
         increment = timedelta(days = 2)
     theDay = theDay + increment
